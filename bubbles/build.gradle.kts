f5430bb95b0da460643475a05ff7583201074dda
Dmitry <kalistratov.d.m@gmail.com>
2025-08-16 04:26:51 +0400

    Add unstyled composable.

--- bubbles/build.gradle.kts
+++ bubbles/build.gradle.kts
@@ -72,6 +72,7 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+            implementation(libs.composables)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
         }
